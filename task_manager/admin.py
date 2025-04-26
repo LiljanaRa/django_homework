69ce230565df31c8afacc6c9a9171951f5bf020d
Liljana <liljana.ratushnyak@gmail.com>
2025-04-26 17:12:01 +0200

    modified file task_manager/admin.py

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.db.models import QuerySet
 
 from task_manager.models import Category, Task, SubTask
 
@@ -10,11 +11,22 @@ class CategoryAdmin(admin.ModelAdmin):
     ordering = ('name',)
 
 
+class SubTaskInline(admin.StackedInline):
+    model = SubTask
+    extra = 1
+
+
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ('title', 'status', 'deadline', 'created_at')
-    search_fields = ('title', 'status', 'deadline', 'created_at' )
+    inlines = [SubTaskInline]
+    list_display = ('short_title', 'status', 'deadline', 'created_at')
+    search_fields = ('short_title', 'status', 'deadline', 'created_at' )
     ordering = ('status', 'deadline')
+
+    def short_title(self, obj: Task):
+        if len(obj.title) > 10:
+            return f"{obj.title[:10]}..."
+        return obj.title
 
 
 @admin.register(SubTask)
@@ -22,6 +34,18 @@ class SubTaskAdmin(admin.ModelAdmin):
     list_display = ('title', 'status', 'deadline', 'created_at')
     search_fields = ('title', 'status', 'deadline', 'created_at')
     ordering = ('status', 'deadline')
+
+    actions = ['update_status_done']
+
+    def update_status_done(self, request, objects: QuerySet):
+        for obj in objects:
+            obj.status = "Done"
+
+            obj.save()
+
+    update_status_done.short_description = "Status done"
+
+
 
 
 
